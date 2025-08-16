f1fc7b93e444c1b7b4dd8cf407a02fa0eb64d7f2
Ivan <gaus.vanya_2006@mail.ru>
2025-08-16 19:26:00 +0300

    update seralizabled models

--- src/main/kotlin/api/Api/Seralizabled.kt
+++ src/main/kotlin/api/Api/Seralizabled.kt
@@ -1,13 +1,15 @@
 package api.Api
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNames
+
 
 @Serializable
 data class HistoryData(
     val date: Long,
-    val amount: Double,
-    val balance: Double,
+    val amount: Float,
+    val balance: Float,
     @JsonNames("to_user_id")
     val toUserId: Long,
     val id: Int,
@@ -20,14 +22,29 @@ data class HistoryData(
 data class InfoData(
     val donateScore: Int? = null,
     val sweets: Int? = null,
-    val commission: Double? = null
+    val golds: Int? = null,
+    val commission: Float? = null
 )
 
 
 @Serializable
 data class BalanceData(
-    val gold: Double,
-    val sweets: Double,
+    val gold: Float,
+    val sweets: Float,
     @JsonNames("donate_score")
     val donateScore: Int,
+)
+
+
+@Serializable()
+data class ResponseResult(
+    val result: JsonElement? = null,
+    val error: APIError? = null
+)
+
+
+@Serializable
+data class APIError(
+    val code: Int? = null,
+    val description: String? = null
 )
