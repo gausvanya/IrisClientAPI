738ab5c481612039dcbe4adf0d2d54613d8c2087
Ivan <gaus.vanya_2006@mail.ru>
2025-09-19 17:56:43 +0300

    add enum class - BotPermissions

--- src/main/kotlin/api/Api/enums.kt
+++ src/main/kotlin/api/Api/enums.kt
@@ -1,7 +1,19 @@
 package IrisClientAPI.Api
 
-enum class Currencies(currency: String) {
+
+enum class Currencies(private val currency: String) {
     GOLD("gold"),
     SWEETS("sweets"),
-    DONATE_SCOPE("donate_score")
+    DONATE_SCOPE("donate_score");
+}
+
+
+enum class BotPermissions(private val info: String) {
+    REG("user_info/reg"),
+    ACTIVITY("user_info/activity"),
+    SPAM("user_info/spam"),
+    STARS("user_info/stars"),
+    POCKET("user_info/pocket");
+
+    fun getValue(): String { return info }
 }
