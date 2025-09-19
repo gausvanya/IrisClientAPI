7d107a902acdf24a7f79440dc6badb8329cfb028
Ivan <gaus.vanya_2006@mail.ru>
2025-09-19 17:59:32 +0300

    parameter total in DetailsHistoryData now Double
    add data classes
    with user information (UserRegInfoResponse, UserActivityInfoResponse, UserActivityResponse, UserSpamInfoResponse, UserStarsInfoResponse, UserSpamChecksResponse, UserPocketInfoResponse, UserPocketResponse)

--- src/main/kotlin/api/Api/Seralizabled.kt
+++ src/main/kotlin/api/Api/Seralizabled.kt
@@ -20,18 +20,18 @@ data class HistoryData(
     val details: DetailsHistoryData? = null,
     val comment: String? = null,
     // val metadata = null,
-    )
+)
 
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class DetailsHistoryData(
-    val total: Int? = null,
+    val total: Double? = null,
     val amount: Int? = null,
     @JsonNames("donate_score")
     val donateScore: Int? = null,
     val fee: Double? = null
-    )
+)
 
 
 @OptIn(ExperimentalSerializationApi::class)
@@ -92,4 +92,74 @@ data class TradesDealsResponse(
     val date: Double,
     val volume: Int,
     val type: String
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserRegInfoResponse(
+    @JsonNames("result")
+    val reg: Long
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserActivityInfoResponse(
+    @JsonNames("result")
+    val activity: UserActivityResponse
+)
+
+
+@Serializable
+data class UserActivityResponse(
+    val total: Int,
+    val week: Int,
+    val month: Int,
+    val day: Int
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserSpamInfoResponse(
+    @JsonNames("result")
+    val spam: UserSpamChecksResponse
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserStarsInfoResponse(
+    @JsonNames("result")
+    val checks: Int
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserSpamChecksResponse(
+    @JsonNames("is_spam")
+    val isSpam: Boolean,
+    @JsonNames("is_ignore")
+    val isIgnore: Boolean,
+    @JsonNames("is_scam")
+    val isScam: Boolean
+)
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+data class UserPocketInfoResponse(
+    @JsonNames("result")
+    val pocket: UserPocketResponse
+)
+
+
+@Serializable
+data class UserPocketResponse(
+    val gold: Int,
+    val coins: Int,
+    val sweets: Double,
+    val stars: Double
 )
